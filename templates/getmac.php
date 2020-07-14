835d30003e165feb887d3426da27f03eba8045f3
trajkd <trajkd@gmail.com>
2020-07-14 12:01:20 +0200

    fix: identation

--- templates/getmac.php
+++
@@ -1,6 +0,0 @@
-<?php
-$MAC = exec('ifconfig en0 | awk "/ether/{print $2}"');
-$MAC = strtok($MAC, ' ');
-$cookie_name = "usermac";
-$cookie_value = $MAC;
-setcookie($cookie_name, $cookie_value, time() + (86400 * 365000), "/");
