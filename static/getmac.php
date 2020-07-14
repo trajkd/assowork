62042b0a89b861e52978307df5dd84fc6bafe5c1
trajkd <trajkd@gmail.com>
2020-07-14 10:39:46 +0200

    fix: set cookie

--- static/getmac.php
+++ static/getmac.php
@@ -1,4 +1,6 @@
 <?php
 $MAC = exec('ifconfig en0 | awk "/ether/{print $2}"');
 $MAC = strtok($MAC, ' ');
-echo "<pre> MAC Address: ".$MAC."</pre>"; 
+$cookie_name = "usermac";
+$cookie_value = $MAC;
+setcookie($cookie_name, $cookie_value, time() + (86400 * 365000), "/");
